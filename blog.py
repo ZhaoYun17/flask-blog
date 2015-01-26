c0ee95d761f73fa0dbfe1637965d8d5152e8e55e
Paxson Lim <potinglim@gmail.com>
2015-01-26 15:50:04 +0800

    Css installed, flask blog done!

--- blog.py
+++ blog.py
@@ -44,9 +44,6 @@ def login():
 			return redirect(url_for('main'))
 	return render_template('login.html', error=error)
 
-
-
-
 @app.route('/main')
 @login_required
 def main():
@@ -62,6 +59,22 @@ def logout():
 	flash('You were logged out')
 	return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash("All fields are required. Please try again.")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into posts (title, post) values (?,?)',
+			[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash('New entry was successfully posted!')
+		return redirect(url_for('main'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
